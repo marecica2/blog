8393486cdd5ece39e141d1d1e591daf98f46fdb8
Marek <marecica2@gmail.com>
2015-08-13 07:24:07 +0200

    if tag

--- src/main/java/org/bmsource/controllers/LoginController.java
+++ src/main/java/org/bmsource/controllers/LoginController.java
@@ -3,8 +3,6 @@ package org.bmsource.controllers;
 import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -13,71 +11,58 @@ import org.bmsource.model.User;
 
 @Named("loggedUser")
 @SessionScoped
-public class LoginController extends BaseController implements Serializable
-{
-    ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+public class LoginController extends BaseController implements Serializable {
 
-    @Inject
-    private UserEJB userEJB;
+	@Inject
+	private UserEJB userEJB;
 
-    private String login;
-    private String password;
+	private String login;
+	private String password;
 
-    private User user;
+	private User user;
 
-    public Boolean isLogged()
-    {
-        if (user != null)
-            return true;
-        return false;
-    }
+	public Boolean isLogged() {
+		if (user != null)
+			return true;
+		return false;
+	}
 
-    public String login()
-    {
-        User user = userEJB.findUserByLogin(login);
-        if (user == null)
-        {
-            System.err.println("User does not exist");
-            return "login.xhtml";
-        }
-        if (!user.getPassword().equals(password))
-        {
-            System.err.println("Incorrect password");
-            return "login.xhtml";
-        }
-        this.user = user;
+	public String login() {
+		User user = userEJB.findUserByLogin(login);
+		if (user == null) {
+			System.err.println("User does not exist");
+		}
+		if (!user.getPassword().equals(password)) {
+			System.err.println("Incorrect password");
+		}
+		if (user != null && user.getPassword().equals(password)) {
+			this.user = user;
+			redirect("/index.xhtml");
+		}
+		return "login.xhtml";
+	}
 
-        redirect("/index.html");
-        return "/index.xhtml";
-    }
+	public User getUser() {
+		return user;
+	}
 
-    public User getUser()
-    {
-        return user;
-    }
+	public void setUser(User user) {
+		this.user = user;
+	}
 
-    public void setUser(User user)
-    {
-        this.user = user;
-    }
+	public String getLogin() {
+		return login;
+	}
 
-    public String getLogin()
-    {
-        return login;
-    }
+	public void setLogin(String login) {
+		this.login = login;
+	}
 
-    public void setLogin(String login)
-    {
-        this.login = login;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public String getPassword()
-    {
-        return password;
-    }
-
-    public void setPassword(String password)
-    {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
