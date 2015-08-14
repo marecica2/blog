2da7ed5ee296893460b31c52449c9b5233c5f819
Marek <marecica2@gmail.com>
2015-08-14 15:20:54 +0200

    login

--- src/main/java/org/bmsource/controllers/LoginController.java
+++ src/main/java/org/bmsource/controllers/LoginController.java
@@ -1,8 +1,16 @@
 package org.bmsource.controllers;
 
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 import org.bmsource.beans.UserEJB;
 import org.bmsource.model.User;
@@ -21,22 +29,48 @@ public class LoginController extends BaseController
 
     private String password;
 
+    private String originalURL;
+
+    @PostConstruct
+    public void init()
+    {
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        originalURL = (String) externalContext.getRequestMap().get(RequestDispatcher.FORWARD_REQUEST_URI);
+
+        if (originalURL == null)
+        {
+            originalURL = externalContext.getRequestContextPath() + "index.xhtml";
+        } else
+        {
+            String originalQuery = (String) externalContext.getRequestMap().get(RequestDispatcher.FORWARD_QUERY_STRING);
+
+            if (originalQuery != null)
+            {
+                originalURL += "?" + originalQuery;
+            }
+        }
+    }
+
     public String login()
     {
         User user = userEJB.findUserByLogin(login);
         if (user == null)
-        {
-            System.err.println("User does not exist");
             errorMessage("loginForm:login", "User does not exist");
-        }
         if (user != null && !user.getPassword().equals(password))
-        {
-            System.err.println("Incorrect password");
             errorMessage("loginForm:password", "Incorrect password");
-        }
         if (user != null && user.getPassword().equals(password))
         {
             userController.setUser(user);
+            ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+            HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
+            try
+            {
+                request.login(login, password);
+                externalContext.redirect(originalURL);
+            } catch (ServletException | IOException e)
+            {
+                e.printStackTrace();
+            }
             return redirect("/index.xhtml");
         }
         return "login.xhtml";
