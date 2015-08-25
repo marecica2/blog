5cbcea62af66800e4d74d10e9f39f8f7966255f8
Marek <marecica2@gmail.com>
2015-08-25 15:14:27 +0200

    jpa test

--- src/test/java/org/bmsource/beans/JPATest.java
+++ src/test/java/org/bmsource/beans/JPATest.java
@@ -8,80 +8,91 @@ import javax.persistence.Persistence;
 
 import org.bmsource.model.Group;
 import org.bmsource.model.User;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-import junit.framework.TestCase;
+public class JPATest
+{
+    private static Logger logger = Logger.getLogger(JPATest.class.getName());
+    private static EntityManagerFactory emFactory;
+    private EntityManager em;
+    private final static String PERSISTENCE_UNIT_NAME = "JEE6Demo-Persistence";
+    private final UserEJB userEJB = new UserEJB();
+    private final GroupEJB groupEJB = new GroupEJB();
 
-public class JPATest extends TestCase {
-	private static Logger logger = Logger.getLogger(JPATest.class.getName());
+    @BeforeClass
+    public static void init()
+    {
+        logger.info("init begin");
+        emFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        logger.info("init end");
+        logger.info("");
+    }
 
-	private EntityManagerFactory emFactory;
+    @AfterClass
+    public static void after()
+    {
+        if (emFactory != null)
+            emFactory.close();
+    }
 
-	private EntityManager em;
+    @Before
+    public void setUp() throws Exception
+    {
+        em = emFactory.createEntityManager();
+        userEJB.setEm(em);
+        groupEJB.setEm(em);
+    }
 
-	private String persistenceUnitName = "JEE6Demo-Persistence";
+    @After
+    public void tearDown() throws Exception
+    {
+        if (em != null)
+            em.close();
+    }
 
-	public JPATest(String testName) {
-		super(testName);
-	}
+    @Test
+    public void testLazy()
+    {
+        System.err.println("testLazy");
+        User u = userEJB.findUserByLogin("test@example.com");
+        userEJB.getEm().detach(u);
+        //Group g = groupEJB.findGroupByName("admins");
+        logger.info(u.toString());
+        //        logger.info(g.toString());
+        logger.info("user " + u.getGroups());
+        //        logger.info("group " + g.getUsers());
+    }
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		try {
-			logger.info("BuildingEntityManager for unit tests");
-			emFactory = Persistence.createEntityManagerFactory(persistenceUnitName);
-			em = emFactory.createEntityManager();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			fail("Exception during JPA EntityManager instanciation.");
-		}
-	}
+    @Test
+    public void testPersistence()
+    {
+        System.err.println("testPersistence");
+        try
+        {
+            User u = new User();
+            u.setFirstName("Marek");
+            u.setLastName("Balla");
+            u.setPassword("long-password");
+            u.setLogin("test@example.com");
 
-	@Override
-	protected void tearDown() throws Exception {
-		super.tearDown();
-		logger.info("Shuting Hibernate JPA layer.");
-		if (em != null) {
-			em.close();
-		}
-		if (emFactory != null) {
-			emFactory.close();
-		}
-		logger.info("Stoppingemory database.");
-	}
+            Group g = new Group();
+            g.setName("admins");
 
-	public void testPersistence() {
-		try {
-			UserEJB userEJB = new UserEJB();
-			userEJB.setEm(em);
-			GroupEJB groupEJB = new GroupEJB();
-			groupEJB.setEm(em);
+            em.getTransaction().begin();
+            userEJB.createUser(u);
+            groupEJB.createGroup(g);
+            u.getGroups().add(g);
+            g.getUsers().add(u);
+            em.getTransaction().commit();
 
-			User u = new User();
-			u.setFirstName("Marek");
-			u.setLastName("Balla");
-			u.setPassword("long-password");
-			u.setLogin("marek@balla.com");
-
-			Group g = new Group();
-			g.setName("admins");
-
-			em.getTransaction().begin();
-			userEJB.createUser(u);
-			groupEJB.createGroup(g);
-			u.getGroups().add(g);
-			g.getUsers().add(u);
-			em.getTransaction().commit();
-
-			u = userEJB.findUserById(u.getId());
-			System.err.println(u);
-			g = groupEJB.findGroupById(g.getId());
-			System.err.println(g);
-
-		} catch (Exception ex) {
-			em.getTransaction().rollback();
-			ex.printStackTrace();
-			fail("Exception during testPersistence");
-		}
-	}
+        } catch (Exception ex)
+        {
+            em.getTransaction().rollback();
+            ex.printStackTrace();
+        }
+    }
 }
