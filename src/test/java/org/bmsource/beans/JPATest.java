9bcb795e5c26eaa9d462f14207210f64720b919f
Marek <marecica2@gmail.com>
2015-08-26 10:49:31 +0200

    hibernate working

--- src/test/java/org/bmsource/beans/JPATest.java
+++ src/test/java/org/bmsource/beans/JPATest.java
@@ -14,75 +14,83 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class JPATest {
-	private static Logger logger = Logger.getLogger(JPATest.class.getName());
-	private static EntityManagerFactory emFactory;
-	private EntityManager em;
-	private final static String PERSISTENCE_UNIT_NAME = "JEE6Demo-Persistence";
-	private final UserEJB userEJB = new UserEJB();
-	private final GroupEJB groupEJB = new GroupEJB();
+public class JPATest
+{
+    private static Logger logger = Logger.getLogger(JPATest.class.getName());
+    private static EntityManagerFactory emFactory;
+    private EntityManager em;
+    private final static String PERSISTENCE_UNIT_NAME = "JEE6Demo-Persistence";
+    private final UserEJB userEJB = new UserEJB();
+    private final GroupEJB groupEJB = new GroupEJB();
 
-	@BeforeClass
-	public static void init() {
-		logger.info("init begin");
-		emFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-		logger.info("init end");
-		logger.info("");
-	}
+    @BeforeClass
+    public static void init()
+    {
+        logger.info("init begin");
+        emFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        logger.info("init end");
+        logger.info("");
+    }
 
-	@AfterClass
-	public static void after() {
-		if (emFactory != null)
-			emFactory.close();
-	}
+    @AfterClass
+    public static void after()
+    {
+        if (emFactory != null)
+            emFactory.close();
+    }
 
-	@Before
-	public void setUp() throws Exception {
-		em = emFactory.createEntityManager();
-		userEJB.setEm(em);
-		groupEJB.setEm(em);
-	}
+    @Before
+    public void setUp() throws Exception
+    {
+        em = emFactory.createEntityManager();
+        userEJB.setEm(em);
+        groupEJB.setEm(em);
+    }
 
-	@After
-	public void tearDown() throws Exception {
-		if (em != null)
-			em.close();
-	}
+    @After
+    public void tearDown() throws Exception
+    {
+        if (em != null)
+            em.close();
+    }
 
-	@Test
-	public void testLazy() {
-		System.err.println("testLazy");
-		User u = userEJB.findUserByLogin("test@example.com");
-		userEJB.getEm().detach(u);
-		Group g = groupEJB.findGroupByName("admins");
-		logger.info(u.toString());
-		logger.info("user " + u.getGroups());
-		logger.info("group " + g.getUsers());
-	}
+    @Test
+    public void testLazy()
+    {
+        System.err.println("testLazy");
+        User u = userEJB.findUserByLogin("test@example.com");
+        Group g = groupEJB.findGroupByName("admins");
+        logger.info(u.toString());
+        logger.info("user " + u.getGroups());
+        logger.info("group " + g.getUsers());
+    }
 
-	@Test
-	public void testPersistence() {
-		System.err.println("testPersistence");
-		try {
-			User u = new User();
-			u.setFirstName("Marek");
-			u.setLastName("Balla");
-			u.setPassword("long-password");
-			u.setLogin("test@example.com");
+    @Test
+    public void testPersistence()
+    {
+        System.err.println("testPersistence");
+        try
+        {
+            User u = new User();
+            u.setFirstName("Marek");
+            u.setLastName("Balla");
+            u.setPassword("long-password");
+            u.setLogin("test@example.com");
 
-			Group g = new Group();
-			g.setName("admins");
+            Group g = new Group();
+            g.setName("admins");
 
-			em.getTransaction().begin();
-			userEJB.createUser(u);
-			groupEJB.createGroup(g);
-			u.getGroups().add(g);
-			g.getUsers().add(u);
-			em.getTransaction().commit();
+            em.getTransaction().begin();
+            userEJB.createUser(u);
+            groupEJB.createGroup(g);
+            u.getGroups().add(g);
+            g.getUsers().add(u);
+            em.getTransaction().commit();
 
-		} catch (Exception ex) {
-			em.getTransaction().rollback();
-			ex.printStackTrace();
-		}
-	}
+        } catch (Exception ex)
+        {
+            em.getTransaction().rollback();
+            ex.printStackTrace();
+        }
+    }
 }
